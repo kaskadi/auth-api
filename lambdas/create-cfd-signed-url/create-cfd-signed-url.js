d12d44caba4bbc2e3309cc49655d66a821eb8da3
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-29 17:19:31 +0200

    Ported old code + refactoring

--- lambdas/create-cfd-signed-url/create-cfd-signed-url.js
+++ lambdas/create-cfd-signed-url/create-cfd-signed-url.js
@@ -1,11 +1,7 @@
+const AWS = require('aws-sdk')
+const getSignedUrl = require('./helpers/get-signed-url.js')
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      message: 'Hello World!'
-    })
-  }
+  const baseResponse = require('auth-api-utils').getBaseResponse()
+  return getSignedUrl(AWS, baseResponse)
 }
